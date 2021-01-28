f90638aeaf60a66263af40c334c83262538097e8
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2021-01-28 17:49:00 -0500

    The card component is styled to include the image, title, collection, and description.

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,22 +1,6 @@
 import React from "react";
 
 function Card(props) {
-    // {props.results.map(result => (
-    // return (
-
-    //             <div className="card" style="width: 18rem;">
-
-    //                 <img src={result.PrimaryImage.Medium} className="card-img-top" alt="...">
-    //                     <div className="card-body">
-    //                         <h5 className="card-title">Card title</h5>
-    //                         <p className="card-text">{result.Culture}</p>
-    //                         <a href="#" className="btn btn-primary">Go somewhere</a>
-
-    //                     </div>
-    //             </div>
-
-    //         )
-    //         ))}
 
     if (props.results) {
         const card = props.results.map(result => {
@@ -24,9 +8,9 @@ function Card(props) {
                 <div className="card" style={{ width: 30 + 'rem' }}>
                     <img className="card-img-top" src={result.PrimaryImage.Medium} />
                     <div className="card-body">
-                        <h1 className="card-title">{result.Culture}</h1>
-                        {/* <h2 className="card-text">{t.location} </h2>
-                        <h4 className="card-text">{t.summary} </h4> */}
+                        <h1 className="card-title">{result.Title}</h1>
+                        <h2 className="card-text">{result.Collection} </h2>
+                        <h4 className="card-text">{result.Description} </h4>
                         <a href="#" className="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
