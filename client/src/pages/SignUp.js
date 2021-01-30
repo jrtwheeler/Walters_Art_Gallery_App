d36c7828cfe8197e386184314a8ff6a4b85fb1f2
pindellk <pindellk@protonmail.com>
2021-01-29 22:14:07 -0500

    Update backend server.js to include passport authentication dependencies, update mongoose connection to specific db with MongoDB Atlas connection string, add authenticate post/get routes

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,14 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function SignUp() {
+  const [registerUsername, setRegisterUsername] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const signup = () => {
+    axios({
+      method: "post",
+      data: {
+        username: registerUsername,
+        password: registerPassword,
+      },
+      withCredentials: true,
+      url: "http://localhost:3000/signup",
+    }).then((res) => console.log(res));
+  };
+  const login = () => {
+    axios({
+      method: "post",
+      data: {
+        username: loginUsername,
+        password: loginPassword,
+      },
+      withCredentials: true,
+      url: "http://localhost:3000/login",
+    }).then((res) => console.log(res));
+  };
+
   return (
     <div>
-      <h1 className="text-center">SignUp Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, est ut enim consequat. Nostrum fastidii partiendo sed ne, no
-        mutat ludus aperiri mea, per in choro dolorem electram. Invidunt reprimique assueverit quo
-        ne, eruditi graecis pro ut. Usu ut diceret scaevola evertitur, appareat voluptatibus ad vel.
-      </p>
+      <div>
+        <h1 className="text-center">Sign Up</h1>
+        <input
+          placeholder="username"
+          onChange={(e) => setRegisterUsername(e.target.value)}
+        />
+        <input
+          placeholder="password"
+          onChange={(e) => setRegisterPassword(e.target.value)}
+        />
+        <button onClick={signup}>Submit</button>
+      </div>
+
+      <div>
+        <h1 className="text-center">Login</h1>
+        <input
+          placeholder="username"
+          onChange={(e) => setLoginUsername(e.target.value)}
+        />
+        <input
+          placeholder="password"
+          onChange={(e) => setLoginPassword(e.target.value)}
+        />
+        <button onClick={login}>Submit</button>
+      </div>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,13 +1,46 @@
 // Establish dependencies
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const password = requires("passport");
+const passportlocal = require("passport-local").Storage;
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.json()); // replaced express with bodyParser
+app.use(bodyParser.urlencoded({ extended: true })); // replaced express with bodyParser
+app.use(
+  cors({
+    origin: "http://localhost:3000", // location of react app we're connecting to
+    credentials: true,
+  })
+);
+
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(cookieParser("secretcode"));
+
+// Authenticate routes
+app.post("/login", (req, res) => {
+  console.log(req.body);
+});
+app.post("/signup", (req, res) => {
+  console.log(req.body);
+});
+app.get("/user", (req, res) => {});
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
@@ -15,11 +48,15 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/collections", {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://pindellk:BhePPs4ozZFMMm6f@cluster0.bwwyi.mongodb.net/collections?retryWrites=true&w=majority",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  }
+);
 
 // Use apiRoutes
 app.use(routes);
