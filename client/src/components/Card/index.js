9729689e7366cc06e498503bd6d88908c467f912
pindellk <pindellk@protonmail.com>
2021-01-31 13:25:11 -0500

    Update /Card index, update passport authenticate - still not re-routing after login

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 function Card(props) {
-<<<<<<< HEAD
   if (props.results) {
     const card = props.results.map((result) => {
       return (
-        <div className="card" style={{ width: 30 + "rem" }}>
+        <div
+          className="card container-fluid mb-3 pt-3"
+          style={{ width: 25 + "rem" }}
+        >
           <img
             className="card-img-top"
             src={result.PrimaryImage.Medium}
@@ -13,6 +15,7 @@ function Card(props) {
           />
           <div className="card-body">
             <h1 className="card-title">{result.Title}</h1>
+            {/* note that "Collection" is "collectionArt" in the db schema */}
             <h2 className="card-text">{result.Collection} </h2>
             <h4>{result.DisplayLocation}</h4>
             <p className="card-text">{result.Description} </p>
@@ -20,40 +23,13 @@ function Card(props) {
               {/* Add modal popout for larger view */}
               See more
             </button>
-            <button href="#" className="btn btn-primary">
-              {/* Add user validation and db action for favorite */}
-              Add to collection
-            </button>
+            <button className="btn btn-primary">Add to collection</button>
           </div>
         </div>
       );
     });
     return <div className="row">{card}</div>;
   }
-=======
-
-    if (props.results) {
-        const card = props.results.map(result => {
-            return (
-                <div className="card container-fluid mb-3 pt-3" style={{ width: 25 + 'rem' }}>
-                    <img className="card-img-top" src={result.PrimaryImage.Medium} alt=""/>
-                    <div className="card-body">
-                        <h1 className="card-title">{result.Title}</h1>
-                        <h2 className="card-text">{result.Collection} </h2>
-                        <h4>{result.DisplayLocation}</h4>
-                        <p className="card-text">{result.Description} </p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            )
-        })
-        return (
-            <div className="row">
-                {card}
-            </div>
-        )
-    }
->>>>>>> 06c31017f0c7036c85f3aa29fa9ae2a802b89e25
 }
 
 export default Card;
--- config/passport.js
+++ config/passport.js
@@ -1,21 +1,31 @@
 // Establish dependences
-const db = require("../models");
 const bcrypt = require("bcryptjs");
 const passport = require("passport");
 const localStrategy = require("passport-local").Strategy;
+const db = require("../models");
 
 module.exports = function (passport) {
   passport.use(
     new localStrategy((username, password, done) => {
       db.User.findOne({ username: username }, (err, user) => {
-        if (err) throw err;
-        if (!user) return done(null, false);
+        if (err) {
+          throw err;
+        }
+        if (!user) {
+          return done(null, false, {
+            message: "Incorrect username",
+          });
+        }
         bcrypt.compare(password, user.password, (err, result) => {
-          if (err) throw err;
+          if (err) {
+            throw err;
+          }
           if (result === true) {
             return done(null, user);
           } else {
-            return done(null, false);
+            return done(null, false, {
+              message: "Incorrect password",
+            });
           }
         });
       });
@@ -23,12 +33,11 @@ module.exports = function (passport) {
   );
 
   passport.serializeUser((user, cb) => {
-    cb(null, user.id);
+    cb(null, user);
   });
 
-  passport.deserializeUser((id, cb) => {
-    db.User.findOne({ _id: id }, (err, user) => {
-      cb(err, user);
-    });
+  // Only send back username in object
+  passport.deserializeUser((obj, cb) => {
+    cb(null, obj);
   });
 };
