90be5d767a9c7ecd8cfef9a2ac4d26faace4541a
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2021-02-07 21:54:33 -0500

    After pushing up a working model, I cleaned up the code to delete console.logs and comments.

--- client/src/components/NavigationBar/index.js
+++ client/src/components/NavigationBar/index.js
@@ -23,58 +23,6 @@ const Styles = styled.div`
   }
 `;
 
-// class NavigationBar extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.handleLoginClick = this.handleLoginClick.bind(this);
-//     this.handleLogoutClick = this.handleLogoutClick.bind(this);
-//     this.state = { isLoggedIn: false };
-//   }
-
-//   handleLoginClick() {
-//     this.setState({ isLoggedIn: true });
-//   }
-
-//   handleLogoutClick() {
-//     this.setState({ isLoggedIn: false });
-//   }
-
-//   render() {
-//     const isLoggedIn = this.state.isLoggedIn;
-//     let button;
-//     if (isLoggedIn) {
-//       button = <LogoutButton onClick={this.handleLogoutClick} />;
-//     } else {
-//       button = <LoginButton onClick={this.handleLoginClick} />;
-//     }
-
-//     return (
-//       <Styles>
-//         <Navbar isLoggedIn={isLoggedIn} expand="lg">
-//           <Navbar.Brand href="/">W.A.G.</Navbar.Brand>
-//           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//           <Navbar.Collapse id="basic-navbar-nav">
-//             <Nav className="ml-auto">
-//               <Nav.Item>
-//                 <Nav.Link href="/collections">My Collections</Nav.Link>
-//               </Nav.Item>
-//               <Nav.Item>
-//                 <Nav.Link href="/signup">Sign Up</Nav.Link>
-//               </Nav.Item>
-//               {button}
-//               {/* <Nav.Item>
-//                 <Nav.Link href="/login">Login</Nav.Link>
-//               </Nav.Item>
-//               {/* // Replaced with a button; add onClickEvent that logs user out and reroutes to landing */}
-//               <Button variant="outline-dark text-light">Logout</Button> */}
-//             </Nav>
-//           </Navbar.Collapse>
-//         </Navbar>
-//       </Styles>
-//     );
-//   }
-// }
-
 class NavigationBar extends Component {
   state = {
     displayValue: "",
@@ -82,7 +30,7 @@ class NavigationBar extends Component {
     error: "",
   };
 
-  componentDidMount() {    
+  componentDidMount() {
     if (localStorage.getItem("username") === null) {
       this.setState({
         displayValue: "Login",
@@ -95,31 +43,31 @@ class NavigationBar extends Component {
       });
     }
 
-    
+
   }
 
-  render () {
-  return (
-    <Styles>
-      <Navbar expand="lg">
-        <Navbar.Brand href="/">W.A.G.</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Item>
-              <Nav.Link href="/collections">My Collections</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="/signup">Sign Up</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href={this.state.route}>{this.state.displayValue}</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </Styles>
-  );
+  render() {
+    return (
+      <Styles>
+        <Navbar expand="lg">
+          <Navbar.Brand href="/">W.A.G.</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Item>
+                <Nav.Link href="/collections">My Collections</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/signup">Sign Up</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href={this.state.route}>{this.state.displayValue}</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Styles>
+    );
   }
 }
 export default NavigationBar;
