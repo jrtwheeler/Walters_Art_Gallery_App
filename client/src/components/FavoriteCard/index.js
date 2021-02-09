8e45bf62aed22c12686281feb9df76fb3cb090e6
pindellk <pindellk@protonmail.com>
2021-02-08 21:47:57 -0500

    Add FavoriteResult component, render results successfully. Need to work on NavBar

--- client/src/components/FavoriteCard/index.js
+++ client/src/components/FavoriteCard/index.js
@@ -1,41 +1,52 @@
-import React, { useState } from "react";
+import React from "react";
 import API from "../../utils/API";
-import "./style.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faEye } from "@fortawesome/free-solid-svg-icons";
+import "./style.css";
 
-function Card(props) {
+function FavoriteCard(props) {
+  console.log(props.user.favorites);
+
   const { user } = props;
-
   const addToFavorites = (favorite) => {
     return (e) => API.updateUser(user._id, { favorites: [favorite] });
   };
 
-  if (props.results) {
-    const card = props.results.map((result, i) => {
+  if (user.favorites) {
+    const favoriteCard = user.favorites.map((favorite, i) => {
+      const slash = "/";
+      const artLink =
+        "https://art.thewalters.org" +
+        slash +
+        "detail" +
+        favorite.ResourceURL.slice(
+          favorite.ResourceURL.lastIndexOf("/"),
+          favorite.ResourceURL.length
+        );
+      console.log(favorite);
       return (
         <div
-          key={`result-${i}`}
+          key={`favorite-${i}`}
           className="container-fluid mb-3 pt-3"
           style={{ width: 12 + "rem", height: "100%" }}
         >
           {/* This is an image/link to go to art.thewalters.org/detail/whateverimagenumber */}
-          <a href={result.ResourceURL} target="_blank" rel="noreferrer">
+          <a href={artLink} target="_blank" rel="noreferrer">
             <img
               className=""
-              src={result.PrimaryImage.Medium}
-              alt={result.Title}
+              src={favorite.PrimaryImage.Medium}
+              alt={favorite.Title}
               variant="primary"
             />
           </a>
           <div className="landingCard pt-3">
             <h6 className="card-title text-center">
-              <strong>{result.Title}</strong>
+              <strong>{favorite.Title}</strong>
             </h6>
 
-            <h6 className="card-text text-center">{result.Creator}</h6>
+            <h6 className="card-text text-center">{favorite.Creator}</h6>
             <h6 className="card-text text-center">
-              <i>{result.Collection}</i>
+              <i>{favorite.Collection}</i>
             </h6>
             {/* <p className="">{result.DisplayLocation}</p> */}
             {/* <p className="card-text">{result.Description} </p> */}
@@ -43,7 +54,7 @@ function Card(props) {
             {/* This is a button/link to go to art.thewalters.org/detail/whateverimagenumber */}
             <div className="text-center p-1">
               <a
-                href={result.ResourceURL}
+                href={artLink}
                 target="_blank"
                 role="button"
                 className="btn btn-light landingBtn"
@@ -56,7 +67,7 @@ function Card(props) {
               <button
                 href="#"
                 className="btn btn-light landingBtn"
-                onClick={addToFavorites(result)}
+                onClick={addToFavorites(favorite)}
               >
                 <h3>
                   <FontAwesomeIcon icon={faPlus} style={{ color: "#4A6479" }} />
@@ -67,8 +78,9 @@ function Card(props) {
         </div>
       );
     });
-    return <div className="row">{card}</div>;
+    return <div className="row">{favoriteCard}</div>;
+  } else {
+    return <p>You don't have any favorites yet!</p>;
   }
 }
-
-export default Card;
+export default FavoriteCard;
