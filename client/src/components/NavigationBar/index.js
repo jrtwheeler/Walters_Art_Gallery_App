7f690e50b1f461ac0ca836b5ad075601a0ca374b
pindellk <pindellk@protonmail.com>
2021-02-08 22:31:54 -0500

    Replace NavigationBar with previous version - no conditional rendering, Logout link shows in NavBar at all times. App works, favorites render when user is logged in

--- client/src/components/NavigationBar/index.js
+++ client/src/components/NavigationBar/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
 import styled from "styled-components";
 
@@ -22,51 +22,31 @@ const Styles = styled.div`
     }
   }
 `;
-class NavigationBar extends Component {
-  state = {
-    displayValue: "",
-    route: "",
-    error: "",
-  };
 
-  componentDidMount() {
-    if (this.props.user === null) {
-      this.setState({
-        displayValue: "Login",
-        route: "/login",
-      });
-    } else {
-      this.setState({
-        displayValue: "Logout",
-        route: "/logout",
-      });
-    }
-  }
-
-  render() {
-    return (
-      <Styles>
-        <Navbar expand="lg">
-          <Navbar.Brand href="/">W.A.G.</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <Nav.Item>
-                <Nav.Link href="/collections">My Collections</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="/signup">Sign Up</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href={this.state.route}>
-                  {this.state.displayValue}
-                </Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </Styles>
-    );
-  }
+function NavigationBar(props) {
+  return (
+    <Styles>
+      <Navbar expand="lg">
+        <Navbar.Brand href="/">W.A.G.</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Item>
+              <Nav.Link href="/collections">My Collections</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/signup">Sign Up</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/login">Login</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/logout">Logout</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Styles>
+  );
 }
 export default NavigationBar;
