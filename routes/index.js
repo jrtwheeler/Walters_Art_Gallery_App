55395581abf1ecf721a44ddcde2d39c8983b5f48
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2021-02-20 12:22:10 -0500

    Fixed the logout route in routes/index.js so the logout re-routes the user to the proper page on logout.

--- routes/index.js
+++ routes/index.js
@@ -40,7 +40,7 @@ router.post("/signup", async (req, res) => {
 router.get("/logout", (req, res) => {
   req.session.destroy(() => {
     // req.logOut();
-    res.redirect("/landing");
+    res.redirect("/");
     res.json(["log", "out"]);
   });
 });
